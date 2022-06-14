25a0bd919ee10ec56294fea1dcc066b4051a5e61
RodrigoDelMoral <rodrigo.delmoral@hotmail.com>
2022-06-14 12:36:13 -0500

    Avances maquetado pagina principal y footer
    Se hicieron cambios en la implementación de la página principal y el header.
    Se maqueto la página principal, considerando que una vez se tenga la BD y los registro esta podrá completarse pues tanto  el carrusel como el catálogo/novedades y anuncios cargarán información desde la BD y estos elementos serán administrables desde el sistema de gestión.
    
    También se agregó el footer.

--- View/paginaPrincipal.php
+++ View/paginaPrincipal.php
@@ -13,16 +13,74 @@
     <link rel="stylesheet" href="../FontAwesome/css/all.css">
 
     <link rel="stylesheet" href="../common/Normalize.css">
+    
     <link rel="stylesheet" href="../common/estilos.css">
 
+    
     <title>Editorial Otro Tipo</title>
 </head>
 
 
 <body>
-   <?php require_once("../common/header.php"); ?>
-   
-   
+    <?php require_once("../common/header.php"); ?>
+
+    <div class="contenedor-main1">
+        <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="true">
+
+            <div class="carousel-indicators">
+                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="3" aria-label="Slide 4"></button>
+                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="4" aria-label="Slide 5"></button>
+
+            </div>
+
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <img src="../img/p1.jpg" class="d-block w-100" alt="slide1">
+                </div>
+                <div class="carousel-item">
+                    <img src="../img/p1.jpg" class="d-block w-100" alt="slide2">
+                </div>
+                <div class="carousel-item">
+                    <img src="../img/p1.jpg" class="d-block w-100" alt="slide3">
+                </div>
+                <div class="carousel-item">
+                    <img src="../img/p1.jpg" class="d-block w-100" alt="slide4">
+                </div>
+                <div class="carousel-item">
+                    <img src="../img/p1.jpg" class="d-block w-100" alt="slide5">
+                </div>
+
+            </div>
+
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Next</span>
+            </button>
+            
+        </div>
+    
+        
+        <div class="contenedorIconos">
+            <a href="#" target="_blank" ><i class="fab fa-instagram iconoRedes"></i></a>
+            <a href="#" target="_blank" ><i class="fab fa-facebook-square iconoRedes"></i></a> 
+            <a href="#" target="_blank" ><i class="fab fa-twitter-square iconoRedes"></i></a>
+            <a href="#" target="_blank" ><i class="fab fa-youtube iconoRedes"></i></a>
+        </div>
+    </div>
+
+    <div id="contenedor-main2">
+        <img src="../img/p1.jpg" alt="" class="anuncios-main">
+        <img src="../img/p1.jpg" alt="" class="anuncios-main">
+    </div>
+
+    <?php require_once("../common/footer.php"); ?>
 
 </body>
 </html>
---
+++ common/footer.php
@@ -0,0 +1,20 @@
+<div class="footer">
+
+    <div class="footer-Section">
+        <a href="">Terminos y condiciones</a>
+        <br>
+        <a href="">Aviso de privacidad</a>
+    </div>
+
+    <div class="footer-Section">
+        <p>2017 Editorial De Otro Tipo S.A de C.V</p>
+    </div>
+
+    <div class="footer-Section">
+        <a href="#" target="_blank" ><i class="fab fa-instagram iconosFooter"></i></a>
+        <a href="#" target="_blank" ><i class="fab fa-facebook-square iconosFooter"></i></a> 
+        <a href="#" target="_blank" ><i class="fab fa-twitter-square iconosFooter"></i></a>
+        <a href="#" target="_blank" ><i class="fab fa-youtube iconosFooter"></i></a>
+    </div>
+
+</div>
