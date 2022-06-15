c3edd5a8ec3597e0d59d103b06c70766464535bf
RodrigoDelMoral <rodrigo.delmoral@hotmail.com>
2022-06-15 16:05:20 -0500

    Enmaquetado Log in

---
+++ View/Admin/Login.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="esp">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Log In de otro tipo</title>
+
+    <link rel="stylesheet" href="../../FontAwesome/css/all.css">
+    <link rel="stylesheet" href="../../common/Normalize.css">
+    <link rel="stylesheet" href="../../common/estilos.css">
+
+
+</head>
+
+<body>
+
+    <header class="header-Login">
+
+        <img class="logo" src="../../Icons/logo.svg" alt="Logo Editorial" width="500" height="600">
+
+        <div class="header-item-Login">
+            <i class="fa  fa-user icono-header-login"></i>
+            <p>Log In De Otro Tipo</p>
+        </div>
+
+    </header>
+
+    <div class="separador-admin"></div>
+
+    <div class="contenedor1-login">
+        <div class="contenedor2-login">
+            <form action="" class="form-login">
+                
+                <div class="formField">
+                    <input type="text" name="userName" id="userName" placeholder="Nombre Usuario">
+                </div>
+
+                <div class="formField">
+                    <input type="password" name="userPass" id="userPass" placeholder="Contraseña">
+                </div>
+
+                <button id="btnLogIn">Iniciar sesión</button>
+
+            </form>
+        </div>
+    </div>
+
+
+
+
+
+
+</body>
+</html>
